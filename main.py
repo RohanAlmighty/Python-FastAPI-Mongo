a62c0af9f5b051dd6b4df3b050ab8464e0daa560
RohanAlmighty <rohanduttaperfect@gmail.com>
2024-10-04 22:05:54 +0530

    👌 IMPROVE: Add exception handling to city_router

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
 import uvicorn
-from routes.routes import router
+from routes.city_router import router as city_router
 
 app = FastAPI()
 
-app.include_router(router)
+app.include_router(city_router, prefix="/cities")
 
 if __name__ == "__main__":
     uvicorn.run(app, port=8005, host="0.0.0.0")
---
+++ routes/city_router.py
@@ -0,0 +1,51 @@
+from fastapi import APIRouter, HTTPException
+from models.cities import City
+from config.database import collection_name
+from schema.schemas import list_serial
+from bson import ObjectId
+
+router = APIRouter()
+
+
+@router.get("/")
+async def get_cities():
+    try:
+        cities = list_serial(collection_name.find())
+        return cities
+    except:
+        raise HTTPException(status_code=500, detail="error fetching cities")
+
+
+@router.post("/")
+async def post_city(city: City):
+    try:
+        collection_name.insert_one(city.model_dump())
+        return {"message": "city added successfully"}
+    except:
+        raise HTTPException(status_code=500, detail="error adding city")
+
+
+@router.put("/")
+async def put_city(id: str, city: City):
+    try:
+        result = collection_name.find_one_and_update(
+            {"_id": ObjectId(id)}, {"$set": city.model_dump()}
+        )
+        if result:
+            return {"message": "city updated successfully"}
+        else:
+            raise HTTPException(status_code=404, detail="city not found")
+    except:
+        raise HTTPException(status_code=500, detail="error updating city")
+
+
+@router.delete("/")
+async def delete_city(id: str):
+    try:
+        result = collection_name.find_one_and_delete({"_id": ObjectId(id)})
+        if result:
+            return {"message": "city deleted successfully"}
+        else:
+            raise HTTPException(status_code=404, detail="city not found")
+    except:
+        raise HTTPException(status_code=500, detail="error deleting city")
